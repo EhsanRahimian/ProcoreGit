a7e21d76f30908e8f18e77d8e623b9662602e53c
Ehsan <ehrath1@gmail.com>
2018-11-04 10:38:20 -0800

    Second Adapter to show Diff and a layout for items of diff_url

---
+++ app/src/main/java/com/nicootech/procoregit/DiffAdapter.java
@@ -0,0 +1,45 @@
+package com.nicootech.procoregit;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class DiffAdapter extends RecyclerView.Adapter<DiffAdapter.MyViewHolder> {
+    private List<String>cards;
+
+    public DiffAdapter(List<String> cards) {
+        this.cards = cards;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parents, int i) {
+        View view = LayoutInflater.from(parents.getContext()).
+                inflate(R.layout.show_diff,parents, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int i) {
+
+        holder.info.setText("List of Differences");
+
+    }
+
+    @Override
+    public int getItemCount() { return cards.size(); }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView info;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            info = itemView.findViewById(R.id.text_diff);
+        }
+    }
+}
